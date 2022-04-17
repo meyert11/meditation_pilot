b609d049f57772d2d798e93d29952bd44c50bce2
Travis <meyert11@gmail.com>
2022-04-17 09:59:26 -0400

    updated Button with Toast, and started implimenting the playing of a short bell in phone's media player

--- app/src/main/java/com/example/meditation_pilot/MainActivity.kt
+++ app/src/main/java/com/example/meditation_pilot/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.meditation_pilot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import android.media.MediaPlayer
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mp: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickme = findViewById<Button>(R.id.button3)
+
+        clickme.setOnClickListener {
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+
+        }
+
+
     }
+    fun playBell(view: android.view.View) {
+        playAudio()
+
+    }
+    private fun playAudio() {
+        mp = MediaPlayer.create(this, R.raw.short_bell)
+        mp.setVolume(0.5f, 0.5f)
+        mp.start()
+    }
+
 }
